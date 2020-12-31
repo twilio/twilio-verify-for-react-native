b6485caf6f3a8e8c52be538132b6246937df40b0
Yeimi Moreno <ymoreno@twilio.com>
2020-12-31 09:56:04 -0500

    Update repo url

--- src/models/PushFactorPayload.ts
+++ src/models/PushFactorPayload.ts
@@ -1,7 +1,7 @@
 // Copyright Twilio, Inc. 2020. All Rights Reserved.
 // Node module: twilio-verify,
 // This file is licensed under the Apache License 2.0.
-// License text available at https://github.com/twilio/twilio-verify-cross-platform/blob/main/LICENSE
+// License text available at https://github.com/twilio/react-native-twilio-verify/blob/main/LICENSE
 
 import type { FactorPayload } from "./FactorPayload";
 import { FactorType } from "./FactorType";
